3ca84b223a5cfa43f20d1de3340221d1c7e31c27
JessIkino <jessicaikino@icloud.com>
2025-01-18 02:38:57 -0500

    Added to the JS to generate a random word in the HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("you're cooked")
+function generatePoem(event){
+
+    event.preventDefault(); 
+
+    new Typewriter("#poem-box", {
+        strings: "La tombe dit à la rose",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+
+} 
+
+let poemFormElement = document.querySelector("#poem-generator")
+poemFormElement.addEventListener("submit", generatePoem)
