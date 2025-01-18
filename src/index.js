6e3533c115b40177a0e6b0a87b10395385c5c48f
JessIkino <jessicaikino@icloud.com>
2025-01-18 16:56:44 -0500

    Added the AI API in this file to help user generate what type of poem they want

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-function generatePoem(event){
-
-    event.preventDefault(); 
-
+function displayPoem(response){
+    console.log("poem generated");
+    
     new Typewriter("#poem-box", {
-        strings: "La tombe dit à la rose",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
+
+}
+
+function generatePoem(event){
+
+    event.preventDefault(); 
+    let instructionInput = document.querySelector("#user-instructions");
+
+    let apiKey = "06cffcec970a84624ftfdo3fb2c867aa";
+    let prompt = `Generate a poem about ${instructionInput .value}`;
+    let context = " Your mission is to generate a basic poem for user in basic HTML do not include the HTML part in the poem, please follow users input, user's input is to generate a lovely poem";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating poem");
+    console.log(`prompt: ${prompt}`);
+    console.log(`context: ${context}`);
+
+
+    axios.get(apiURL).then(displayPoem);
+
+
 
 } 
 
